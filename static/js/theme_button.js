0687f35087feb200d8eb52e76f32ce1f8109ba16
GabrielDTB <gabriel@gabrieltb.me>
2023-11-10 00:06:14 -0500

    feat: add transition for theme switch
    fdgdf

--- static/js/theme_button.js
+++ static/js/theme_button.js
@@ -1,7 +1,16 @@
 // switch theme mode
 if (document.getElementById('mode')) {
     document.getElementById('mode').addEventListener('click', () => {
+    var timeout = .1;
+    var style = document.createElement('style');
+    style.innerHTML = `
+        * {
+        transition: background-color ${timeout}s ease-out,color ${timeout}s ease-out
+        }
+    `;
+    document.head.appendChild(style);
     document.documentElement.classList.toggle('switch');
     localStorage.setItem('theme', document.documentElement.classList.contains('switch') ? 'switch' : 'default');
+    setTimeout(() => document.head.removeChild(style), timeout * 1000);
   });
 }
